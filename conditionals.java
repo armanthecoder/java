62f810b71602d13ba782dc2bb72dadc78300d148
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-11 18:58:57 +0530

    Create conditionals.java

---
+++ conditionals.java
@@ -0,0 +1,18 @@
+public class conditionals_cwh {
+    public static void main(String[]args){
+        int age =10;
+       // if(age>18){
+        // System.out.println("Yes boy you can drive now!");
+       // }
+        //else{
+          //  System.out.println("No boy you cannot drive yet!");
+        //}
+        boolean cond= (age==18);
+        if(cond){
+            System.out.println("Yes boy you can drive now!");
+        }
+        else{
+            System.out.println("No boy you cannot drive yet!");
+        }
+    }
+}
