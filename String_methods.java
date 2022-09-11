4b1c3277f25b75756d9eed97cb80d889a77438ff
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-11 18:56:08 +0530

    Create String_methods.java

---
+++ String_methods.java
@@ -0,0 +1,43 @@
+public class string_methods {
+    public static void main(String[]args){
+        String name="arman malik";
+        int value = name.length();
+        System.out.println(value);  // length of string
+
+
+        //String lstring = name.toLowerCase();  // To lowercase
+        //System.out.println(lstring);
+
+
+        //String ustring =name.toUpperCase();  // to upper case
+        //System.out.println(ustring);
+
+
+        //String nontrimmedString = "   Harry   ";
+        //System.out.println(nontrimmedString);  //.trim
+
+
+        //String trimmedstring = nontrimmedString.trim();
+        //System.out.println(trimmedstring);
+        //System.out.println(name.substring(1));   //name.substring
+        //System.out.println(name.substring(1,9));
+
+
+        //System.out.println(name.replace('m', 'h')); //name.replace
+        //System.out.println(name.replace("r", "ap"));//name.replace
+
+
+        //System.out.println(name.startsWith("prm"));  // using boolean to declare whether string starts with the entered char or not
+        //System.out.println(name.endsWith("lik"));  //name.endswith
+
+
+       //System.out.println(name.charAt(4));  //name.charAt
+
+        //System.out.println(name.indexOf("m"));  //name.indexOf
+       //System.out.println(name.lastIndexOf("rm",2));
+
+
+        //System.out.println(name.equals("arman malik"));
+        //System.out.println(name.equalsIgnoreCase("arman malik"));
+    }
+}
