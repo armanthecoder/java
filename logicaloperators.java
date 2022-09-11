6f29c29f0fe820b417c63b37fce3b2cae47bd480
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-11 18:55:02 +0530

    Create logicaloperators.java

---
+++ logicaloperators.java
@@ -0,0 +1,35 @@
+public class logical_cwh {
+    public static void main(String[]args){
+        //boolean a = true;
+        //boolean b = true;
+        //System.out.println("For logical AND...");
+        //if(a && b){
+        //System.out.println("Y");
+        //}
+        //else{
+        //System.out.println("N");
+        //}
+
+
+        // boolean a = true;
+        //boolean b = false;
+        //System.out.println("For logical OR..");
+        //if(a || b){
+        //System.out.println("Y");
+        //}
+        //else{
+        //System.out.println("N");
+        //}
+
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println("For logical NOT...");
+        System.out.print("Not (a) is ");
+        System.out.println(!a);
+        System.out.print("Not (b) is  ");
+        System.out.println(!b);
+        System.out.println();
+        }
+    }
+
