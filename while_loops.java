6363b48518ac275b4209c1589c599676b75ba133
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-12 07:22:55 +0530

    Create while_loops.java

---
+++ while_loops.java
@@ -0,0 +1,28 @@
+public class while_loops_cwh {
+    public static void main(String[]args){
+       /* System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("Printing using while loops");
+        int i=1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running while loop!");
+        */
+
+          // infinite loop example
+         // while(true){
+         // System.out.println("I am infinitely printing 1");
+         //}
+
+
+        //quick quiz
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
