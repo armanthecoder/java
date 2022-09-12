69b9f8594b0b8b19707b967e69b20521f873bb3a
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-12 07:23:34 +0530

    Create do_while_loops.java

---
+++ do_while_loops.java
@@ -0,0 +1,17 @@
+public class do_while_loops {
+    public static void main(String[]args){
+        //int a = 0;
+        //do{
+        //    System.out.println(a);
+        //    a++;
+        //}while(a<=5);
+
+
+        //quick quiz
+        int c=1;
+        do{
+            System.out.println(c);
+            c++;
+        }while(c<=45);
+    }
+}
