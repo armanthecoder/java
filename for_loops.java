659ab0dc2a4b66ea953fab71f1fedf532483c89e
ARMAN MALIK <111848360+armanthecoder@users.noreply.github.com>
2022-09-14 14:49:59 +0530

    Create for_loops.java

---
+++ for_loops.java
@@ -0,0 +1,29 @@
+public class for_loops {
+    public static void main(String[]args){
+   //     for(int i=0;i<=10;i++){
+   //             System.out.println(i);
+   //     }
+
+
+        // quick quiz1
+        //2*i=0,2,4,6,8
+        //2*i+1=1,3,5,7,9
+
+        //int n=20;
+        //for(int i=0; i<n; i++){
+        //    System.out.println(2*i+1);
+        //}
+
+
+        // example for decrementing function in for loop
+        //for(int i=5;i>=0;i--){
+        //    System.out.println(i);
+        //}
+
+        //quick quiz2
+        int n=18;
+        for(int i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
